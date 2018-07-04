d5c3ce99637b73a954008100f606ad8cf6223ff2
Awwab Tahir <awwabtahir11@gmail.com>
2018-07-04 12:03:24 +0500

    removed production db 7/4/2018

--- api/models/db.js
+++ api/models/db.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://asasa:marwat!123@ds117739.mlab.com:17739/mydbasasa';
-if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGOLAB_URI;
-}
+// if (process.env.NODE_ENV === 'production') {
+//   dbURI = process.env.MONGOLAB_URI;
+// }
 
 mongoose.connect(dbURI);
 
